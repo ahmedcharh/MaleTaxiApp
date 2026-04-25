c8036090aa34e9cea20f43b9a57edf41f12b6d4b
ahmedcharh <ahmed.charh@gmail.com>
2026-04-25 23:34:28 +0500

    Add MapView component code

---
+++ src/components/MapView.js
@@ -0,0 +1,13 @@
+// MapView Component Code
+import React from 'react';
+
+const MapView = () => {
+    return (
+        <div>
+            <h2>Map View</h2>
+            {/* Map Integration Code Goes Here */}
+        </div>
+    );
+};
+
+export default MapView;
